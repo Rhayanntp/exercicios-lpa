853d07e4eaa4630392c39a4b6396b5a3ed3dc35f
Rhayanntp <rhayanrhangel58@gmail.com>
2024-03-08 10:37:50 -0300

    Exercicio 11

--- routes/api.php
+++ routes/api.php
@@ -72,3 +72,10 @@ Route::get('receber/primeiroNumero', function(Request $request){
     $resultado = $primeiroNumero * 2;
     return 'O dobro do '. $primeiroNumero . 'é igual a' . $resultado;
 });
+
+Route::get('calcular/base/altura', function(Request $request){
+    $base = $request->input('base');
+    $altura = $request->input('altura');
+    $resultado = $base * $altura;
+    return $resultado;
+});
