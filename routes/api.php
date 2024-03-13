918f2e239e7606fdadff1dd15eb69a288ae4ea4d
Rhayanntp <rhayanrhangel58@gmail.com>
2024-03-13 15:26:16 -0300

    Criei 17 exercícios

--- routes/api.php
+++ routes/api.php
@@ -4,71 +4,71 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('receber/nome', function(Request $request){
-    $nome = $request->input('$nome');
+    $nome = $request->input('nome');
     return $nome;
 });
 
 Route::get('receber/nome/idade', function(Request $request){
-    $nome = $request->input('$nome');
-    $idade = $request->input('$idade');
+    $nome = $request->input('nome');
+    $idade = $request->input('idade');
     return $nome . $idade;
 });
 
 Route::get('receber/nome/nascimento/cidadeNatal', function(Request $request){
-    $nome = $request->input('$nome');
-    $nascimento = $request->input('$nascimento');
-    $cidadeNatal = $request->input('$cidadeNatal');
+    $nome = $request->input('nome');
+    $nascimento = $request->input('nascimento');
+    $cidadeNatal = $request->input('cidadeNatal');
     return $nome . $nascimento . $cidadeNatal;
 });
 
-Route::get('receber/primeiroNumero/segundoNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero');
-    $segundoNumero = $request->input('$segundoNumero');
-    $resultado = $primeiroNumero . $segundoNumero;
+Route::get('exercicio4', function(Request $request){
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
+    $resultado = $primeiroNumero + $segundoNumero;
     return $resultado;
 });
 
-Route::get('receber/primeiroNumero/segundoNumero/terceiroNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero');
-    $segundoNumero = $request->input('$segundoNumero');
-    $terceiroNumero = $request->input('$terceiroNumero');
-    $resultado = $primeiroNumero . $segundoNumero . $terceiroNumero;
+Route::get('receber/1Numero/2Numero/terceiroNumero', function(Request $request){
+    $primeiroNumero = $request->input('1Numero');
+    $segundoNumero = $request->input('2Numero');
+    $terceiroNumero = $request->input('terceiroNumero');
+    $resultado = $primeiroNumero - $segundoNumero - $terceiroNumero;
     return $resultado;
 });
 
-Route::get('receber/primeiroNumero/segundoNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero'); 
-    $segundoNumero = $request->input('$segundoNumero'); 
+Route::get('receber/Numero1/Numero2', function(Request $request){
+    $primeiroNumero = $request->input('Numero1'); 
+    $segundoNumero = $request->input('Numero2'); 
     $resultado =  $primeiroNumero / $segundoNumero;
     return $resultado; 
 });
 
-Route::get('receber/primeiroNumero/segundoNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero'); 
-    $segundoNumero = $request->input('$segundoNumero'); 
-    $resultado =  $segundoNumero . $primeiroNumero;
+Route::get('receber/primeiro/segundo', function(Request $request){
+    $primeiroNumero = $request->input('primeiro'); 
+    $segundoNumero = $request->input('segundo'); 
+    $resultado =  $primeiroNumero * $segundoNumero;
     return $resultado; 
 });
 
 Route::get('receber/primeiroNumero/segundoNumero/terceiroNumero/quartoNumero/quintoNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero'); 
-    $segundoNumero = $request->input('$segundoNumero'); 
-    $terceiroNumero = $request->input('$terceiroNumero');
-    $quartoNumero = $request->input('$quartoNumero');
-    $quintoNumero = $request->input('$quintoNumero');
+    $primeiroNumero = $request->input('primeiroNumero'); 
+    $segundoNumero = $request->input('segundoNumero'); 
+    $terceiroNumero = $request->input('terceiroNumero');
+    $quartoNumero = $request->input('quartoNumero');
+    $quintoNumero = $request->input('quintoNumero');
     $resultado =  $primeiroNumero + $segundoNumero; + $terceiroNumero +  $quartoNumero +  $quintoNumero / 5;
     return $resultado; 
 });
 
-Route::get('receber/primeiroNumero/segundoNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero'); 
-    $segundoNumero = $request->input('$segundoNumero'); 
+Route::get('receber/primeiro1/segundo2', function(Request $request){
+    $primeiroNumero = $request->input('primeiro1'); 
+    $segundoNumero = $request->input('segundo2'); 
     $resultado =  $segundoNumero / $primeiroNumero;
     return $resultado; 
 });
 
-Route::get('receber/primeiroNumero', function(Request $request){
-    $primeiroNumero = $request->input('$primeiroNumero');
+Route::get('receber/1primeiro', function(Request $request){
+    $primeiroNumero = $request->input('1primeiro');
     $resultado = $primeiroNumero * 2;
     return 'O dobro do '. $primeiroNumero . 'é igual a' . $resultado;
 });
@@ -92,9 +92,8 @@ Route::get('calcular/valor/desconto/valorDodesconto/valorComdesconto', function(
 Route::get('calcular/salario/salarioAumento/aumento', function(Request $request){
     $salario = $request->input('salario');
     $aumento = $request->input('aumento');
-    $salarioAumento = $salario + $aumento;
-    $resultado = $salarioAumento;
-    return $resultado;
+    $salarioAumento = ($aumento / 100) * $salario;
+    return $salarioAumento + $salario;
 });
 
 Route::get('receber/valor/calcular/pontos', function(Request $request){
@@ -115,7 +114,7 @@ Route::get('calcular/dias/horas/minutos/segundos', function(Request $request){
     $horas = $dias * 24;
     $minutos = $dias *  1440;
     $segundos = $dias *  86400;
-    $resultado = $horas . $minutos . $segundos;
+    $resultado = $horas .'horas'. $minutos .'minutos'. $segundos .'segundos';
     return $resultado;
 });
 
